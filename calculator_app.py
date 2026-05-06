9831f558bba7e46aebc46c8e4e66d7c2a6778acf
jenamndz <jenamndz18@gmail.com>
2026-05-06 22:59:40 +0800

    shows the user the list of operations they can choose and asks them to pick what they want

--- calculator_app.py
+++ calculator_app.py
@@ -3,5 +3,11 @@ def calculator():
     while True:
         print("\n--- Simple App Calculator ---")
 
+        print("Operations: \n"
+              "1. Addition \n"
+              "2. Subtraction \n"
+              "3. Multiplication \n"
+              "4. Division")
+        choice = input("Enter choice (1/2/3/4): ")
 
 calculator()

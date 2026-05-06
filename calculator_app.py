90f5a0f34e7da9c4bbd8611d5902f78684540b3d
jenamndz <jenamndz18@gmail.com>
2026-05-06 23:01:24 +0800

    checks if the user entered a valid choice, if yes, the programs asks the user to input two numbers

--- calculator_app.py
+++ calculator_app.py
@@ -10,4 +10,8 @@ def calculator():
               "4. Division")
         choice = input("Enter choice (1/2/3/4): ")
 
+        if choice in ['1', '2', '3', '4']:
+            try:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
 calculator()
